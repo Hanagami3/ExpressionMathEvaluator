b50ee46a0bcd43e4e9a100c6847987c74cc94e0a
Hanagami <hanaga3mi@gmail.com>
2024-01-15 01:29:11 +0100

    15/01

---
+++ src/main/java/be/intecbrussel/MathExpressionEvaluatorTest/MathApp.java
@@ -0,0 +1,8 @@
+package be.intecbrussel.MathExpressionEvaluatorTest;
+
+public class MathApp {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/be/intecbrussel/MathExpressionEvaluatorTest/service/BasicMathServiceTest.java
@@ -0,0 +1,28 @@
+package be.intecbrussel.MathExpressionEvaluatorTest.service;
+
+    import be.intecbrussel.MathExpressionEvaluatorTest.Service.BasicMathService;
+    import be.intecbrussel.MathExpressionEvaluatorTest.Service.BasicMathServiceImpl;
+    import org.junit.jupiter.api.*;
+
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    public class BasicMathServiceTest {
+        private BasicMathService basicMathService;
+
+        @BeforeAll
+        public void beforeAll() {
+            // TODO instantiate the service
+            basicMathService = new BasicMathServiceImpl();
+        }
+
+        @Test
+        public void testBasicAdditionOfTwoIntegers() {
+            int firstNumber = 7;
+            int secondNumber = 6;
+
+            int expectedValue = 13;
+
+            double result = basicMathService.add(firstNumber, secondNumber);
+
+            Assertions.assertEquals(expectedValue, result);
+        }
+    }
